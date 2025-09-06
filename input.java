e79a3c0aa59133f9b605683605e8df6c030d57cb
Tanveer1907 <1906tanveer@gmail.com>
2025-09-06 10:44:22 +0530

    Java practice started

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number:");
+//        int age = sc.nextInt();
+//        System.out.println("Enter second number:");
+//        int age2 = sc.nextInt();
+//        int sum = age +age2;
+//        System.out.print("Sum = ");
+//        System.out.println(sum);
+        String str = sc.nextLine();
+        System.out.println(str);
+
+    }
+}
---
+++ percentage_cal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class percentage_cal {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("DS:");
+        float DS = sc.nextInt();
+        System.out.println("OOSE:");
+        float OOSE = sc.nextInt();
+        System.out.println("DBMS:");
+        float DBMS = sc.nextInt();
+        System.out.println("FEE:");
+        float FEE = sc.nextInt();
+        System.out.println("IOT:");
+        float IOT = sc.nextInt();
+
+        double total = DS+ OOSE+DBMS+FEE+IOT;
+        double average = total/5;
+
+        System.out.println(average);
+    }
+
+}
