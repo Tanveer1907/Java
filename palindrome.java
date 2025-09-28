3915cc61fc25b368f726ceb13f0da436547392de
Tanveer1907 <1906tanveer@gmail.com>
2025-09-28 12:06:19 +0530

    palindrome

---
+++ palindrome.java
@@ -0,0 +1,20 @@
+//public class palindrome {
+//    static int reverse(int num, int rev){
+//        if(num == 0){
+//            return rev;
+//        }
+//        return reverse(num/10, rev*10+num%10);
+//    }
+//    public static void main (String[] args){
+////        int num = 166;
+////        int rev = reverse(num,0);
+////        if(num == rev){
+////            System.out.println("Palindrome");
+////        }
+////        else{
+////            System.out.println("Not palindrome");
+////        }
+//    }
+//}
+
+
